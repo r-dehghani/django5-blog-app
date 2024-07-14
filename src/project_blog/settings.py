33f10f0b34a8d15340245e937af974d0168f1453
dariush dehghani <r.dehghani.90@gmail.com>
2024-07-14 23:07:16 +0330

    create model for posts

--- src/project_blog/settings.py
+++ src/project_blog/settings.py
@@ -31,12 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
+    # -------- built-in apps ----------
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # --------- installed apps ----------
+    'blog_app.apps.BlogAppConfig',
 ]
 
 MIDDLEWARE = [
